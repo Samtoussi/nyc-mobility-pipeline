e7af999cfe5e163904992fdb672251eced734ac6
Samtoussi <saml.toussi@gmail.com>
2026-08-22 21:07:07 +0200

    Fix GitHub OIDC trust policy

--- infrastructure/github_oidc.tf
+++ infrastructure/github_oidc.tf
@@ -34,7 +34,7 @@ resource "aws_iam_role" "github_actions" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = "repo:Samtoussi/nyc-mobility-pipeline:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:Samtoussi@179637459/nyc-mobility-pipeline@1335888278:ref:refs/heads/main"
           }
         }
       }
